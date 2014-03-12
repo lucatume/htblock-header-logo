296afe7bad7a4d1611354a9ab9d98a95ce0b388d
Luca Tumedei <luca@theaveragedev.com>
2014-03-12 08:48:00 +0100

    commented out all the methods and options

--- includes/headerlogo/Block.php
+++ includes/headerlogo/Block.php
@@ -8,41 +8,41 @@ namespace headerlogo;
             public $options_class = '\headerlogo\BlockOptions';
             public $description = 'An Headway block that displays the site logo, title and tagline and allows for some user customization.';
 
-            function enqueue_action($block_id) {
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+            // function enqueue_action($block_id) {
+            //     /* CODE FOR OPTIONAL METHOD */
+            //     return;
+            // }
 
-            function init_action($block_id) {
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+            // function init_action($block_id) {
+            //     /* CODE FOR OPTIONAL METHOD */
+            //     return;
+            // }
 
-            function dynamic_js($block_id) {
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+            // function dynamic_js($block_id) {
+            //     /* CODE FOR OPTIONAL METHOD */
+            //     return;
+            // }
 
-            function dynamic_css($block_id) {               
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+            // function dynamic_css($block_id) {               
+            //     /* CODE FOR OPTIONAL METHOD */
+            //     return;
+            // }
 
-            function setup_elements() {
-                $this->register_block_element(array(
-                    'id' => 'element1-id',
-                    'name' => 'Element 1 Name',
-                    'selector' => '.my-selector1',
-                    'properties' => array('property1', 'property2', 'property3'),
-                    'states' => array(
-                        'Selected' => '.my-selector1.selected',
-                        'Hover' => '.my-selector1:hover',
-                        'Clicked' => '.my-selector1:active'
-                    )
-                ));
-            }
+            // function setup_elements() {
+            //     $this->register_block_element(array(
+            //         'id' => 'element1-id',
+            //         'name' => 'Element 1 Name',
+            //         'selector' => '.my-selector1',
+            //         'properties' => array('property1', 'property2', 'property3'),
+            //         'states' => array(
+            //             'Selected' => '.my-selector1.selected',
+            //             'Hover' => '.my-selector1:hover',
+            //             'Clicked' => '.my-selector1:active'
+            //         )
+            //     ));
+            // }
 
-            function content($block) {
-                /* CODE HERE */
-            }
+            // function content($block) {
+            //     /* CODE HERE */
+            // }
         }
--- includes/headerlogo/BlockOptions.php
+++ includes/headerlogo/BlockOptions.php
@@ -4,19 +4,19 @@ namespace headerlogo;
     class BlockOptions extends \HeadwayBlockOptionsAPI {
 
         public $tabs = array(
-            'my-only-tab' => 'Settings'
+            // 'my-only-tab' => 'Settings'
         );
 
         public $inputs = array(
 
-            'my-only-tab' => array(
+            // 'my-only-tab' => array(
 
-                'text-input' => array(
-                    'type' => 'image',
-                    'name' => 'logo',
-                    'label' => 'Logo',
-                    'tooltip' => 'Put anything relevant about the input in this tooltip.'
-                )
-            )
+            //     'text-input' => array(
+            //         'type' => 'image',
+            //         'name' => 'logo',
+            //         'label' => 'Logo',
+            //         'tooltip' => 'Put anything relevant about the input in this tooltip.'
+            //     )
+            // )
         );
     }
