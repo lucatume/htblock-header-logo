5ae7e14364fb0368bc356560501641fbf28b4114
Luca Tumedei <luca@theaveragedev.com>
2014-03-13 09:52:22 +0100

    added static method templating as per Headway core code

--- includes/headerlogo/Block.php
+++ includes/headerlogo/Block.php
@@ -12,48 +12,30 @@ class Block extends \HeadwayBlockAPI
     public $description = 'An Headway block that displays the site logo, title and tagline and allows for some user customization.';
     protected $themeSupport;
 
-    public function __construct()
+    public function enqueue_action($block_id)
     {
         // create an instance of the ThemeSupport class
         $this->themeSupport = new ThemeSupport();
         $this->themeSupport->add('custom-header');
+        return;
     }
 
-            // function enqueue_action($block_id) {
-            //     /* CODE FOR OPTIONAL METHOD */
-            //     return;
-            // }
+    // public static function init_action($block_id, $block)
+    // {
+    // }
 
-            // function init_action($block_id) {
-            //     /* CODE FOR OPTIONAL METHOD */
-            //     return;
-            // }
 
-            // function dynamic_js($block_id) {
-            //     /* CODE FOR OPTIONAL METHOD */
-            //     return;
-            // }
-
-            // function dynamic_css($block_id) {
-            //     /* CODE FOR OPTIONAL METHOD */
-            //     return;
-            // }
-
-            // function setup_elements() {
-            //     $this->register_block_element(array(
-            //         'id' => 'element1-id',
-            //         'name' => 'Element 1 Name',
-            //         'selector' => '.my-selector1',
-            //         'properties' => array('property1', 'property2', 'property3'),
-            //         'states' => array(
-            //             'Selected' => '.my-selector1.selected',
-            //             'Hover' => '.my-selector1:hover',
-            //             'Clicked' => '.my-selector1:active'
-            //         )
-            //     ));
-            // }
-
-            // function content($block) {
-            //     /* CODE HERE */
-            // }
+    // public static function enqueue_action($block_id, $block, $original_block = null)
+    // {
+    // }
+    
+    
+    // public static function dynamic_css($block_id, $block, $original_block = null)
+    // {
+    // }
+    
+    
+    // public static function dynamic_js($block_id, $block, $original_block = null)
+    // {
+    // }
 }
