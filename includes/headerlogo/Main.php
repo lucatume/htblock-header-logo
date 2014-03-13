451bb7390a17ff769ec8fd951fa7f8b0a08ef862
Luca Tumedei <luca@theaveragedev.com>
2014-03-13 16:50:46 +0100

    added the custom header theme support

--- includes/headerlogo/Main.php
+++ includes/headerlogo/Main.php
@@ -1,25 +1,35 @@
 <?php
 namespace headerlogo;
 
+use tad\wrappers\ThemeSupport;
+
 class Main
 {
-    public function __construct()
+    protected $themeSupport;
+
+    public function __construct(\tad\wrappers\ThemeSupport $themeSupport = null)
     {
         add_action('after_setup_theme', array($this, 'blockRegister'));
         add_action('init', array($this, 'extend_updater'));
+        // add custom header image support
+        $this->themeSupport = new ThemeSupport();
+        $this->themeSupport->add('custom-header');
     }
 
     public function blockRegister()
     {
-        if ( !class_exists('Headway') )
+        if (!class_exists('Headway')) {
             return;
+        }
         return headway_register_block('\headerlogo\Block', plugins_url(false, __FILE__));
     }
 
 
-    public function extend_updater() {
-        if ( !class_exists('HeadwayUpdaterAPI') )
+    public function extend_updater()
+    {
+        if (!class_exists('HeadwayUpdaterAPI')) {
             return;
+        }
         $updater = new \HeadwayUpdaterAPI(array(
             'slug' => 'headerlogo',
             'path' => plugin_basename(__FILE__),
