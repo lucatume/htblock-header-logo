224d37165b625b50aef4e37d1701cbeaea0735f4
Luca Tumedei <luca@theaveragedev.com>
2014-03-14 12:42:43 +0100

    first iteration of the block taking both users options into account; still not using the image position options

--- includes/headerlogo/Block.php
+++ includes/headerlogo/Block.php
@@ -20,14 +20,49 @@ class Block extends \HeadwayBlockAPI
     // public static function enqueue_action($block_id, $block, $original_block = null)
     // {
     // }
-    
-    
+
+
     // public static function dynamic_css($block_id, $block, $original_block = null)
     // {
     // }
-    
-    
+
+
     // public static function dynamic_js($block_id, $block, $original_block = null)
     // {
     // }
+    public function content($block)
+    {
+    // get the theme user choice about the header image
+    // defaults to false
+        $headerImage = get_theme_mod('header_image', 'remove-header');
+        $imageSrc = $headerImage;
+        if ($headerImage == 'remove-header') {
+            // the user does not want to show an header image
+            // does theme developer wishes to use a default image?
+            // options for select controls are saved by their index
+            // show no image is the 0 index option
+            $useDefaultImage = \HeadwayBlocksData::get_block_setting($block, 'no-user-image');
+            if (!$useDefaultImage) {
+                // nothing will be printed to the page but before and
+                // after filter will be fired the same
+                $this->theHeader();
+            }
+            // the image source is set to default image one
+            // not checking if the theme developer actually set it or not
+            $imageSrc = \HeadwayBlocksData::get_block_setting($block, 'default-image-src');
+        }
+        // echo the markup to the page
+        $this->theHeader($imageSrc);
+    }
+    protected function theHeader($imageSrc = null)
+    {
+        do_action('headway_before_header_link');
+        // no image source then do not print the image markup to the page
+        if (!is_null($imageSrc)) {
+            echo '<a href="' . home_url() . '" class="banner-image">';
+            echo '<img src="' . $imageSrc . '" alt="' . get_bloginfo('name') . '" />';
+            echo '</a>';
+        }
+        do_action('headway_after_header_link');
+    }
 }
