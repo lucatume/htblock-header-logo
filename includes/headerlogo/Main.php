aab233b54a4a1f43be84231e20d683bd66017efd
Luca Tumedei <luca@theaveragedev.com>
2014-03-14 16:53:34 +0100

    added the section to register the visual editor panel

--- includes/headerlogo/Main.php
+++ includes/headerlogo/Main.php
@@ -14,8 +14,23 @@ class Main
         // add custom header image support
         $this->themeSupport = new ThemeSupport();
         $this->themeSupport->add('custom-header');
+        // register this block theme-wide options
+        // but do it after headway theme setup
+        add_action('after_setup_theme', array($this, 'addVisualEditorPanels'));
     }
-
+    public function addVisualEditorPanels()
+    {
+        // include the class defining those options
+        include_once dirname(__FILE__) . '/HeaderImagePanel.php';
+        // register the visual editor panel
+        $class = '\headerlogo\HeaderImagePanel';
+        $tag = 'headway_visual_editor_display_init';
+        // hook in with a priority higher than the one Headway registers
+        // its own setup block to have the Header Image options panel show
+        // on the right side of it
+        $priority = 1000;
+        add_action($tag, create_function('', 'return headway_register_visual_editor_panel_callback(\'' . $class . '\');'), $priority);
+    }
     public function blockRegister()
     {
         if (!class_exists('Headway')) {
